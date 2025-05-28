fab6e6bed01570da6f39eb23be630aea47e45d94
Israel-ferreira77 <178834968+Israel-ferreira77@users.noreply.github.com>
2025-05-27 21:37:31 -0300

    atualizacao
    atualizando codigo

--- srcc/gerente.java
+++ srcc/gerente.java
@@ -1,5 +1,18 @@
-gerente.java
+main/
+│
+├── Main.java
+└── src/
+    ├── models/
+    │   ├── Cliente.java
+    │   ├── Funcionario.java
+    │   └── Gerente.java
+    └── service/
+        ├── ClienteService.java
+        ├── FuncionarioService.java
+        ├── GerenteService.java
+        └── PedidoService.java
 
+        
 import main.src.models.Cliente;
 import main.src.models.Funcionario;
 import main.src.models.Gerente;
