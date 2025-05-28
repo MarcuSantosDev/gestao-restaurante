4dd4adbd9de187f4e5310dd5e45ff419e4e2a754
Israel-ferreira77 <178834968+Israel-ferreira77@users.noreply.github.com>
2025-05-27 21:33:51 -0300

    interface, teste, integracao
    interface, teste, integracao

---
+++ srcc/gerente.java
@@ -0,0 +1,9 @@
+gerente.java
+
+import main.src.models.Cliente;
+import main.src.models.Funcionario;
+import main.src.models.Gerente;
+import main.src.service.ClienteService;
+import main.src.service.FuncionarioService;
+import main.src.service.GerenteService;
+import main.src.service.PedidoService;
